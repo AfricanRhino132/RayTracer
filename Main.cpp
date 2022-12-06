6dca7af37cd977651b44d0303f9d3328c5c357f3
AfricanRhino132 <ryanlarson382@gmail.com>
2022-12-06 15:26:49 -0700

    we drawing stuff in this

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,17 @@
 #include "Renderer/Renderer.h"
+#include "Objects/Sphere.h"
 #include <iostream>
 
 int main(int, char**)
 {
 	Renderer renderer;
 	renderer.Initialize();
-	renderer.CreateWindow(400, 300);
+	renderer.CreateWindow(600, 300);
+
+	Canvas canvas(600, 300, renderer);
+
+	auto sphere = std::make_unique<Sphere>(glm::vec3{ 0, 0, 1 }, 0.5f, nullptr);
+
 	bool quit = false;
 	while (!quit)
 	{
@@ -16,7 +22,23 @@ int main(int, char**)
 		case SDL_QUIT:
 			quit = true;
 			break;
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				quit = true;
+				break;
+			}
+			break;
 		}
+
+		// render scene
+		canvas.Clear({ 0, 0, 0, 1});
+		renderer.Render(canvas, sphere.get());
+		canvas.Update();
+
+		renderer.CopyCanvas(canvas);
+		renderer.Present();
 	}
 	renderer.Shutdown();
 	return 0;
--- Renderer/Renderer.h
+++ Renderer/Renderer.h
@@ -1,13 +1,29 @@
 #pragma once
+#include "Canvas.h"
+#include "../Math/Ray.h"
 #include <SDL.h>
+
+class Object;
+
 class Renderer
 {
 public:
 	Renderer() = default;
+
+	void Render(Canvas& canvas, Object* object);
+
 	bool Initialize();
 	void Shutdown();
 	bool CreateWindow(int width, int height);
+
+	void CopyCanvas(const Canvas& canvas);
+	void Present();
+
+	friend class Canvas;
+
 private:
 	SDL_Window* m_window{ nullptr };
 	SDL_Renderer* m_renderer{ nullptr };
+
+	color3 GetBackgroundFromRay(const Ray& ray);
 };
