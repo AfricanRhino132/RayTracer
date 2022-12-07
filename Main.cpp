f9c4f5842c195bc158cb71fd195acd7872239be3
AfricanRhino132 <ryanlarson382@gmail.com>
2022-12-07 16:26:55 -0700

    Metal

--- Main.cpp
+++ Main.cpp
@@ -6,16 +6,21 @@
 
 int main(int, char**)
 {
+	const int width = 800;
+	const int height = 600;
+	const int samples = 100;
+
 	Renderer renderer;
 	renderer.Initialize();
-	renderer.CreateWindow(600, 300);
+	renderer.CreateWindow(width, height);
 
-	Canvas canvas(600, 300, renderer);
-	Camera camera({ 0, 1, 2 }, { 0, 0, 0 }, { 0, 1, 0 }, 70.0f, 600 / (float)300);
+	Canvas canvas(width, height, renderer);
+	Camera camera({ 0, 1, 2 }, { 0, 0, 0 }, { 0, 1, 0 }, 70.0f, width / (float)height);
 	Scene scene;
 
-	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ -0.75f, 0.0f, -1.0f }, 0.5f, std::make_unique<Lambertian>(color3{ 0, 1, 0 })));
-	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0.75f, 0.0f, -1.0f }, 0.5f, std::make_unique<Lambertian>(color3{ 0, 0, 1 })));
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ -1.25f, 0.0f, -1.0f }, 0.5f, std::make_unique<Lambertian>(color3{ 0, 1, 0 })));
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 1.25f, 0.0f, -1.0f }, 0.5f, std::make_unique<Metal>(color3{ 1, 1, 1 }, 0.3f)));
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0.0f, 0.0f, -1.0f }, 0.5f, std::make_unique<Lambertian>(color3{ 1, 0, 0 })));
 	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0.0f, -100.5f, -1.0f }, 100.0f, std::make_unique<Lambertian>(color3{ 0.2f })));
 
 	bool quit = false;
@@ -40,7 +45,7 @@ int main(int, char**)
 
 		// render scene
 		canvas.Clear({ 0, 0, 0, 1});
-		renderer.Render(canvas, scene, camera, 100);
+		renderer.Render(canvas, scene, camera, samples);
 		canvas.Update();
 
 		renderer.CopyCanvas(canvas);
--- Renderer/Material.h
+++ Renderer/Material.h
@@ -14,8 +14,17 @@ public:
     Lambertian(const color3& albedo) : m_albedo{ albedo } {}
     bool Scatter(const Ray& ray, const RaycastHit& hit, color3& attenuation, Ray& scattered) const override;
 
+protected:
+    color3 m_albedo;
+};
 
+class Metal : public Material
+{
+public:
+    Metal(const color3& albedo, float fuzz = 0) : m_albedo{ albedo }, m_fuzz { fuzz } {}
+    bool Scatter(const Ray& ray, const RaycastHit& hit, color3& attenuation, Ray& scattered) const override;
 
 protected:
     color3 m_albedo;
+    float m_fuzz;
 };
